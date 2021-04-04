64dffc9e9f9448d54ed0fc4ac1801d35d2894f17
David <david.cabello.info@gmail.com>
2021-04-04 19:17:46 +0200

    fin Sección 5

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -19,7 +19,9 @@ export class DbzService {
     return [...this._personajes];
   }
 
-  constructor(){
-    console.log('Servicio inicializado');
+  constructor(){}
+
+  agregarPersonaje( personaje: Personaje) {
+      this._personajes.push( personaje );
   }
 }
